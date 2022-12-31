c31e4290b02c8055216e8caef773f1ff22519699
Alexey2610 <aleksej26101998@gmail.com>
2022-12-31 19:38:16 +0300

    My first ArrayList
    Practice homework(Arraylist)

---
+++ DkArray/src/Main.java
@@ -0,0 +1,16 @@
+import javax.naming.Name;
+import java.util.ArrayList;
+
+import java.util.jar.Attributes;
+
+public class Main {
+    public static void main(String[] args) {
+    ArrayList<String>names = new ArrayList<String>();
+    names.add("Serun");
+    names.add("Banan");
+    names.add("Antonov");
+    System.out.println(names);
+
+
+    }
+}
